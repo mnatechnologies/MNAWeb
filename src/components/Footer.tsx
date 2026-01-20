1d0bdacb25be177ccd62bf21533450cb329f183c
Mark <mark@mnatechnologies.com.au>
2026-01-20 19:42:03 +1100

    Initial scaffold + content added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -93,6 +93,14 @@ export default function Footer() {
                   Contact
                 </Link>
               </li>
+              <li>
+                <Link href="/brochure" className="text-gray-400 hover:text-white transition-colors flex items-center gap-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+                  </svg>
+                  Services Brochure
+                </Link>
+              </li>
             </ul>
           </div>
 
